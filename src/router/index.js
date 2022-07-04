8708126a6b0da8eaffb18c66e4b6d73407a1c033
chris <c56984200123@gmail.com>
2022-07-04 22:11:49 +0800

    update avatar image & remove about page

--- src/router/index.js
+++ src/router/index.js
@@ -23,14 +23,6 @@ const router = createRouter({
       component: EncodeDecodeView,
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
-    },
-    {
       path: "/:pathMatch(.*)*",
       name: "error",
       component: ErrorView,
